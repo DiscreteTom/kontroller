9e959228a1c3e346b0422f5b3df21befc5deebc1
DiscreteTom <discrete_tom@outlook.com>
2023-10-21 19:37:31 +0800

    chore: extract input_thread

---
+++ src/input_thread.rs
@@ -0,0 +1,65 @@
+use std::{thread, time::Duration};
+use steamworks::{Client, SingleClient};
+
+fn pool<T, F>(single: &SingleClient, interval_ms: u64, f: F) -> T
+where
+  F: Fn() -> Option<T>,
+{
+  loop {
+    single.run_callbacks();
+
+    match f() {
+      Some(v) => {
+        return v;
+      }
+      None => {}
+    }
+
+    thread::sleep(Duration::from_millis(interval_ms));
+  }
+}
+
+pub fn spawn() {
+  thread::spawn(|| {
+    // TODO: replace 480 with the real AppID
+    let (client, single) = Client::init_app(480).unwrap();
+
+    let input = client.input();
+    input.init(false);
+
+    let (fps_controls_action_set, attack_action_handle, attack2_action_handle) =
+      pool(&single, 100, || {
+        let fps_controls_action_set = input.get_action_set_handle("FPSControls");
+        let attack_action_handle = input.get_digital_action_handle("attack");
+        let attack2_action_handle = input.get_digital_action_handle("attack2");
+
+        if fps_controls_action_set != 0 && attack_action_handle != 0 && attack2_action_handle != 0 {
+          println!("action set ready");
+          return Some((
+            fps_controls_action_set,
+            attack_action_handle,
+            attack2_action_handle,
+          ));
+        }
+        return None;
+      });
+
+    let input_handles = pool(&single, 300, || {
+      let handles = input.get_connected_controllers();
+      if !handles.is_empty() {
+        println!("num of input handles: {:?}", handles.len());
+        return Some(handles);
+      }
+      return None;
+    });
+
+    input.activate_action_set_handle(input_handles[0], fps_controls_action_set);
+
+    // TODO: replace 1000 with the real interval
+    pool(&single, 1000, || {
+      let value = input.get_digital_action_data(input_handles[0], attack_action_handle);
+      println!("attack: {}", value.bState);
+      Option::<()>::None // run forever
+    });
+  });
+}
